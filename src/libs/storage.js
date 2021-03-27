ec6fef888f9a6d8a3feab3bbd2f6d68dce38108d
MarqCervMartin <martin_mcervantes@hotmail.com>
2021-03-26 23:26:20 -0600

    Creando Storage

---
+++ src/libs/storage.js
@@ -0,0 +1,50 @@
+import AsyncStorage from '@react-navigation/stack'
+
+class Storage{
+    static instance = Storage();
+
+    store = async(key, value) =>{
+        try {
+            await AsyncStorage.setItem(key, value);
+
+            return true;
+        } catch (error) {
+            console.log("storage store err", error)
+            return false;
+        }
+    }
+    get = async (key) =>{
+        try {
+            return await AsyncStorage.getItem(key);
+        } catch (error) {
+            console.log("Storage get err", error)
+            throw Error(error);
+        }
+    }
+    multiGet = async(keys) =>{
+        try {
+            return await AsyncStorage.multiGet(keys);
+        } catch (error) {
+            console.log("Storage multiget err", error)
+            throw Error(error);
+        }
+    }
+    getAllkeys = async() =>{
+        try {
+            return await AsyncStorage.getAllkeys();
+        } catch (error) {
+            console.log("Storage getAllkeys err", error)
+            throw Error(error);
+        }
+    }
+
+    remove = async(key) =>{
+        try {
+            await AsyncStorage.removeItem(key);
+            return true;
+        } catch (error) {
+            console.log("Storage remove err", error)
+            return false;
+        }
+    }
+}
